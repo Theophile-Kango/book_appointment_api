188e51e4867048e849d9c7abce0ab58965cf3dfd
Theophile-Kango <fadhili.kango@gmail.com>
2020-09-15 15:11:32 +0200

    Add the image attachment to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
-class User < ApplicationRecord
+class User < ApplicationRecordh
+    has_one_attached :image
     has_many :appointments, dependent: :destroy
     has_many :doctors, through: :appointments
 end
