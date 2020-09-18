70b4912413c0abea97d33435a35986fa45f2923d
Theophile-Kango <fadhili.kango@gmail.com>
2020-09-18 18:26:10 +0200

    Add test for doctors_spec

--- app/lib/message.rb
+++ app/lib/message.rb
@@ -30,4 +30,8 @@ class Message
     def self.expired_token
         'Sorry, your token has expired. Please login to continue.'
     end
+
+    def self.no_admin
+      'Sorry you must be an admin to perform the requested action'
+    end
 end
